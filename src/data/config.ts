f2d2ce8637daf448741d6a6d2588d0a07995372b
smakosh <ismai23l@hotmail.com>
2021-05-30 12:56:00 -0700

    chore: expose NEXT_PUBLIC_VERCEL_URL and remove the env var

--- src/data/config.ts
+++ src/data/config.ts
@@ -2,7 +2,7 @@ const config = {
   defaultTitle: 'John Doe',
   url:
     process.env.NODE_ENV !== 'development'
-      ? process.env.NEXT_PUBLIC_PORTFOLIO_URL
+      ? process.env.NEXT_PUBLIC_VERCEL_URL
       : 'http://localhost:3040',
   defaultDescription: 'I’m John and I’m a Backend & Devops engineer!',
   googleAnalyticsID: 'G-PE8QW0ZGTZ',
