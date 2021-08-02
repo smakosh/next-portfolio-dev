f47bdcc9aefa9621f75ee89788d5d9b6f9789988
smakosh <ismai23l@hotmail.com>
2021-08-02 05:07:21 -0700

    chore: use Script component

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,12 +1,5 @@
 import { ServerStyleSheet } from 'styled-components';
-import Document, {
-  Html,
-  Head,
-  Main,
-  NextScript,
-  DocumentContext,
-} from 'next/document';
-import config from 'data/config';
+import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document';
 
 class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
@@ -15,8 +8,7 @@ class MyDocument extends Document {
     try {
       ctx.renderPage = () =>
         originalRenderPage({
-          enhanceApp: (App) => (props) =>
-            sheet.collectStyles(<App {...props} />),
+          enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />),
         });
       const initialProps = await Document.getInitialProps(ctx);
       return {
@@ -37,49 +29,15 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-          <link
-            rel="apple-touch-icon"
-            sizes="180x180"
-            href="/assets/favicon/apple-touch-icon.png"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="32x32"
-            href="/assets/favicon/favicon-32x32.png"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="16x16"
-            href="/assets/favicon/favicon-16x16.png"
-          />
+          <link rel="apple-touch-icon" sizes="180x180" href="/assets/favicon/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/assets/favicon/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/assets/favicon/favicon-16x16.png" />
           <link rel="manifest" href="/assets/favicon/site.webmanifest" />
-          <link
-            rel="mask-icon"
-            href="/assets/favicon/safari-pinned-tab.svg"
-            color="#5bbad5"
-          />
+          <link rel="mask-icon" href="/assets/favicon/safari-pinned-tab.svg" color="#5bbad5" />
           <link rel="shortcut icon" href="/assets/favicon/favicon.ico" />
           <meta name="msapplication-TileColor" content="#da532c" />
-          <meta
-            name="msapplication-config"
-            content="/assets/favicon/browserconfig.xml"
-          />
+          <meta name="msapplication-config" content="/assets/favicon/browserconfig.xml" />
           <meta name="theme-color" content="#6b63ff" />
-	  <script async src={`https://www.googletagmanager.com/gtag/js?id=${config.googleAnalyticsID}`}></script>
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-  		window.dataLayer = window.dataLayer || [];
-  		function gtag(){dataLayer.push(arguments);}
-			gtag('js', new Date());
-			gtag('config', '${config.googleAnalyticsID}', {
-				page_path: window.location.pathname,
-			});
-  		`,
-            }}
-          />
         </Head>
         <body>
           <Main />
