2a41461d5530ec34d856dcd3350d5ecd547b374a
joshower <josehowerupn@gmail.com>
2021-09-16 15:04:27 +0200

    Add url based on environtmet

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -1,6 +1,12 @@
+import { Utility } from '../support/utility';
+
+// Call getBaseUrl() to get environment specific url value
+const url = new Utility().getBaseUrl();
+
 describe('Can find the different components in the app', () => {
   it('can visit home page and load all page content', () => {
-    cy.visit('/');
+    console.log(`This is the url ${url}`);
+    cy.visit(url);
     cy.get('[data-cy="intro"]').should('be.visible');
     cy.get('[data-cy="projects"]').should('be.visible');
     cy.get('[data-cy="about"]').should('be.visible');
