1e27c7562df02f95ea5c8545194e6248d395c6f7
joshower <josehowerupn@gmail.com>
2021-09-16 15:10:28 +0200

    Add log to check url

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -5,7 +5,7 @@ const url = new Utility().getBaseUrl();
 
 describe('Can find the different components in the app', () => {
   it('can visit home page and load all page content', () => {
-    console.log(`This is the url ${url}`);
+    cy.log(`This is the url ${url}`);
     cy.visit(url);
     cy.get('[data-cy="intro"]').should('be.visible');
     cy.get('[data-cy="projects"]').should('be.visible');
