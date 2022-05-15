b582810260e1837970932d83de2396338b0bec24
smakosh <ismai23l@hotmail.com>
2022-05-15 02:38:43 +0200

    chore: opt-in swc for minification and add aria label to theme toggle

---
+++ next.config.js
@@ -0,0 +1,10 @@
+// @ts-check
+
+/**
+ * @type {import('next').NextConfig}
+ **/
+const nextConfig = {
+  swcMinify: true,
+};
+
+module.exports = nextConfig;
--- src/components/ui/theme/Header/ToggleTheme/index.tsx
+++ src/components/ui/theme/Header/ToggleTheme/index.tsx
@@ -10,6 +10,7 @@ const ToggleTheme = () => {
       className="text-left lg:text-center bg-none border-none cursor-pointer transition-all duration-300 focus:transition-all focus:duration-300"
       type="button"
       onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}
+      aria-label="Toggle theme"
     >
       {theme === 'dark' ? (
         <Sun className="text-black dark:text-white lg:dark:text-black" />
