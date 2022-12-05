b58c2d814311753128a55cb66eb5dd08a2d74d73
José Fernando Höwer Barbosa <josehowerupn@gmail.com>
2022-12-05 16:59:10 +0100

    Add workflows

---
+++ cypress/e2e/navigation.cy.js
@@ -0,0 +1,11 @@
+import { cy } from 'local-cypress';
+
+describe('Can find the different components in the app', () => {
+  it('can visit home page and load all page content', () => {
+    cy.visit('/');
+    cy.get('[data-cy="intro"]').should('be.visible');
+    cy.get('[data-cy="projects"]').should('be.visible');
+    cy.get('[data-cy="about"]').should('be.visible');
+    cy.get('[data-cy="contact"]').should('be.visible');
+  });
+});
