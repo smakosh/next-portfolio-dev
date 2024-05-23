7c76db541d3b95427c420e3892d78c7b47ccf5bf
smakosh <ismai23l@hotmail.com>
2024-05-23 19:15:39 +0200

    chore: remove sleep

--- src/lib/getRepos.ts
+++ src/lib/getRepos.ts
@@ -2,13 +2,8 @@ import 'server-only';
 import { RepositoryEdge } from 'generated/graphql';
 import { unstable_noStore as noStore } from 'next/cache';
 
-function sleep(ms: number) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-}
-
 const getRepos = async (): Promise<RepositoryEdge[]> => {
   noStore();
-  await sleep(2000);
   const res = await fetch('https://api.github.com/graphql', {
     method: 'POST',
     body: JSON.stringify({
